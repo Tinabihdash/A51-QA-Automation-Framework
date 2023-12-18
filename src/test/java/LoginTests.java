c8287e29038320566351025a04c6cda7fd347f4f
TinaBihdash <valentyna.bihdash@testpro.io>
2023-12-18 13:18:56 -0500

    homework22
    homework22

--- src/test/java/LoginTests.java
+++ src/test/java/LoginTests.java
@@ -1,5 +1,5 @@
-import Pages.HomePage;
-import Pages.LoginPage;
+import pages.HomePage;
+import pages.LoginPage;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
