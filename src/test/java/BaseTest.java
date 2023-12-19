b3bbccf74993745efc44ca5c6ac143d8a36392af
TinaBihdash <valentyna.bihdash@testpro.io>
2023-12-19 16:46:01 -0500

    homework24
    homework24

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -4,16 +4,21 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.*;
 
+import java.net.MalformedURLException;
+import java.net.URI;
 import java.time.Duration;
 
 public class BaseTest {
-    public WebDriver driver = null;
+    public static WebDriver driver = null;
     public String url = "https://qa.koel.app/";
     public WebDriverWait wait =null;
    Actions actions;
@@ -24,13 +29,13 @@ public class BaseTest {
     }
    @BeforeMethod
    @Parameters({"BaseURL"})
-    public void launchBrowser(String BaseURL){
+    public void launchBrowser(String BaseURL) throws MalformedURLException {
+driver =pickBrowser(System.getProperty("browser"));
+      // ChromeOptions options = new ChromeOptions();
+      // options.addArguments("--remote-allow-origins=*");
 
-       ChromeOptions options = new ChromeOptions();
-       options.addArguments("--remote-allow-origins=*");
-
-       driver  = new ChromeDriver(options);
-       driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+      // driver  = new ChromeDriver(options);
+       //driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
        wait =new WebDriverWait(driver,Duration.ofSeconds(10));
 actions =new Actions(driver);
        url =BaseURL;
@@ -42,6 +47,28 @@ actions =new Actions(driver);
     }
     public void navigateLoginPage(){
         //driver.get(url);
+
+    }
+    public static WebDriver pickBrowser(String browser) throws MalformedURLException {
+        DesiredCapabilities caps = new DesiredCapabilities();
+        String gridUrl = "http://10.0.0.206:4444";
+
+       switch (browser){
+           case "firefox":
+               WebDriverManager.firefoxdriver().setup();
+               return driver = new FirefoxDriver();
+           case"grid-edge": //gradle clean test -Dbrowser=grid-edge
+          caps.setCapability("browser", "MicrosoftEdge");
+          return driver = new RemoteWebDriver(URI.create(gridUrl).toURL(),caps);
+           case"grid-chrome": //gradle clean test -Dbrowser=grid-chrome
+               caps.setCapability("browserName", "chrome");
+               return driver = new RemoteWebDriver(URI.create(gridUrl).toURL(),caps);
+           default:
+               WebDriverManager.chromedriver().setup();
+               ChromeOptions options = new ChromeOptions();
+               options.addArguments("--remote-allow-origins=*");
+               return driver=new ChromeDriver(options);
+       }
 
     }
 
