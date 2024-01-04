380fadbc33b9a50f598f23c4e5a892466c0543ab
TinaBihdash <valentyna.bihdash@testpro.io>
2024-01-04 11:41:46 -0500

    Update LoginPage.java

--- src/test/java/pages/LoginPage.java
+++ src/test/java/pages/LoginPage.java
@@ -3,29 +3,57 @@ package pages;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.interactions.Actions;
-import org.openqa.selenium.support.PageFactory;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.support.FindBy;
 
-import java.time.Duration;
-
-public class BasePage {
-    WebDriver driver;
-    WebDriverWait wait;
-
-    Actions actions;
-
-    public BasePage(WebDriver givenDriver){
-        driver = givenDriver;
-        wait = new WebDriverWait(driver, Duration.ofSeconds(5));
-        actions = new Actions(driver);
-        PageFactory.initElements(driver,this);
-
+public class LoginPage extends BasePage{
+    public LoginPage(WebDriver givenDriver) {
+        super(givenDriver);
     }
 
-    public WebElement findElement (By locator){
-        return wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
+    //Locators PageFactory
+    @FindBy(css = "input[type='email']")
+    WebElement emailTextField;
+    @FindBy(css = "input[type='password']")
+    WebElement passwordTextField;
+    @FindBy(css = "button[type='submit']")
+    WebElement submitLoginButton;
+
+    //helper methods for PageFactory
+    public void provideEmailToLogin(String email){
+
+        emailTextField.sendKeys(email);
+    }
+    public void providePasswordToLogin(String password) {
+
+        passwordTextField.sendKeys(password);
+    }
+
+    public void clickSubmitToLogin() {
+
+        submitLoginButton.click();
+    }
+
+    //Locators
+
+    By emailField = By.cssSelector("input[type='email']");
+
+    By passwordField =By.cssSelector("input[type='password']");
+    By submit =By.cssSelector("button[type='submit']");
+
+    public void provideEmail(String email){
+        findElement(emailField).sendKeys(email);
+    }
+    public void providePassword(String password) {
+        findElement(passwordField).sendKeys(password);
+    }
+
+    public void clickSubmit() {
+        findElement(submit).click();
+    }
+    public void login(){
+        provideEmail("valentyna.bihdash@testpro.io");
+        providePassword("TestTest1!");
+        clickSubmit();
     }
 }
 
