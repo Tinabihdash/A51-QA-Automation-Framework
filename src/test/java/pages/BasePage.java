99bc39d31893d3c1a0144c5e19db2d2a7db226c9
TinaBihdash <valentyna.bihdash@testpro.io>
2024-01-04 11:41:18 -0500

    Update BasePage.java

--- src/test/java/pages/BasePage.java
+++ src/test/java/pages/BasePage.java
@@ -3,14 +3,28 @@ package pages;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
-public class HomePage extends BasePage{
-    public HomePage(WebDriver givenDriver) {
-        super(givenDriver);
+import java.time.Duration;
+
+public class BasePage {
+    WebDriver driver;
+    WebDriverWait wait;
+
+    Actions actions;
+
+    public BasePage(WebDriver givenDriver){
+        driver = givenDriver;
+        wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+        actions = new Actions(driver);
+        PageFactory.initElements(driver,this);
+
     }
-    By userAvatarIcon = By.cssSelector("img.avatar");
 
-    public WebElement getUserAvatar(){
-        return  findElement(userAvatarIcon);
+    public WebElement findElement (By locator){
+        return wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
     }
 }
