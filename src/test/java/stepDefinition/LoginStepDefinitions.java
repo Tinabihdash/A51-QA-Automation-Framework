9097dd2ae376470c44e05d3e96c12aca9328b6bc
TinaBihdash <valentyna.bihdash@testpro.io>
2024-01-18 14:44:20 -0500

    homework-28
    homework-28

--- src/test/java/stepDefinition/LoginStepDefinitions.java
+++ src/test/java/stepDefinition/LoginStepDefinitions.java
@@ -1,29 +1,68 @@
 package stepDefinition;
 
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 
 import java.time.Duration;
 
 public class LoginStepDefinitions {
     WebDriver driver;
     WebDriverWait wait;
+    //private ChromeOptions options;
 
-    //("i open browser")
-    public void openBrowser(){
-        WebDriverManager.chromedriver().setup();
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--disable-notifications");
-        options.addArguments("--remote-allow-origins");
-        driver=new ChromeDriver(options);
-        wait =  new WebDriverWait(driver, Duration.ofSeconds(10));
+
+    @Before
+            public void openBrowser(){
+            WebDriverManager.chromedriver().setup();
+            ChromeOptions options = new ChromeOptions();
+            options.addArguments("--disable-notifications");
+            options.addArguments("--remote-allow-origins");
+            driver=new ChromeDriver(options);
+            wait =  new WebDriverWait(driver, Duration.ofSeconds(10));
+            }
+
+@After
+public void closeBrowser(){
+        driver.quit();
+
+}
+    @Given("I open login page")
+    public void iOpenLoginPage() {
+        driver.get("https://qa.koel.app/");
     }
 
+    @And("I enter email {string}")
+    public void iEnterEmail(String email) {
+            wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("[type='email']"))).sendKeys(email);
+    }
 
+    @And("I enter password {string}")
+    public void iEnterPassword(String password) {
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("[type='password']"))).sendKeys(password);
+    }
 
+    @And("I submit")
+    public void iSubmit() {
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("[type='submit']"))).click();
+    }
+
+    @Then("I am logged in")
+    public void iAmLoggedIn() {
+        Assert.assertTrue(wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("img.avatar"))).isDisplayed());
+    }
 }
 
 
--- src/test/resources/features/login.feature
+++ src/test/resources/features/login.feature
@@ -1,8 +1,7 @@
 Feature: Login feature
 
   Scenario: Login Success
-    Given I open browser
-    And I open login page
+    Given I open login page
     And I enter email "valentyna.bihdash@testpro.io"
     And I enter password "TestTest1!"
     And I submit
